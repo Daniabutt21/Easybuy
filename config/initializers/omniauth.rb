442c0d672fead08ccfd0fa9a29b2cd712eb79979
Daniabutt21 <dania.asrar@devsinc.com>
2023-08-29 02:03:45 -0700

    Fixed rubocop issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 OmniAuth.config.on_failure = proc do |env|
-  env["devise.mapping"] = Devise.mappings[:user]
-  "Users::OmniauthCallbacksController".constantize.action(:failure).call(env)
+  env['devise.mapping'] = Devise.mappings[:user]
+  'Users::OmniauthCallbacksController'.constantize.action(:failure).call(env)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,24 @@
 
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'pages/home'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
 
-    devise_scope :user do
-      get "/users/auth/google_oauth2" => "users/omniauth_callbacks#google_oauth2"
-    end
-  
-    devise_scope :user do
-      get 'login', to: 'devise/sessions#new'
-    end
+  devise_scope :user do
+    get '/users/auth/google_oauth2' => 'users/omniauth_callbacks#google_oauth2'
+  end
 
-    devise_scope :user do
-      get 'signup', to: 'devise/registrations#new'
-    end
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
 
   resources :products
 end
